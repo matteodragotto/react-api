00164a245a65d3097a717d4774f2cd9a23dddc87
matteodragotto <matteodrag8@gmail.com>
2025-01-22 13:08:15 +0100

    [feat] post card structure - axios import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import axios from "axios"
+import { useState, useEffect } from "react"
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className="container my-5">
+      <h1 className="text-center">Il mio blog</h1>
+      <div className="row">
+        <div className="col-12 col-md-4">
+          <div className="card">
+            <img src="" alt="" />
+            <div className="card-body">
+              <h5 className="card-title">
+                Qui va il titolo del post
+              </h5>
+              <p className="card-text">
+                Qui va la descrizione del post
+              </p>
+              <p className="card-text">Qui vanno i tag</p>
+              <div className="btn btn-danger">
+                <i className="fa-solid fa-trash-can"></i> Elimina
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
