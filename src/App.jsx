02e9bd99d6fdf0466cb59e8503f000df7ea4b0f5
matteodragotto <matteodrag8@gmail.com>
2025-01-22 14:16:27 +0100

    [feat] card generation with api data

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,53 @@ import axios from "axios"
 import { useState, useEffect } from "react"
 
 const App = () => {
+
+  const postApiUrl = 'http://localhost:3001'
+  const [posts, setPosts] = useState([])
+
+  const fetchPosts = () => {
+
+    axios.get(`${postApiUrl}/posts`)
+      .then(res => {
+        console.log(res.data);
+        setPosts(res.data)
+      })
+      .catch(error => {
+        console.error('Errore durante il caricamento dei post:', error)
+      })
+
+  }
+
+
+  useEffect(() => {
+    fetchPosts()
+  }, [])
+
   return (
     <div className="container my-5">
       <h1 className="text-center">Il mio blog</h1>
       <div className="row">
-        <div className="col-12 col-md-4">
-          <div className="card">
-            <img src="" alt="" />
-            <div className="card-body">
-              <h5 className="card-title">
-                Qui va il titolo del post
-              </h5>
-              <p className="card-text">
-                Qui va la descrizione del post
-              </p>
-              <p className="card-text">Qui vanno i tag</p>
-              <div className="btn btn-danger">
-                <i className="fa-solid fa-trash-can"></i> Elimina
+        {posts.map(post => (
+          <div className="col-12 col-md-4">
+            <div className="card">
+              <img src={post.image} alt={post.title} />
+              <div className="card-body">
+                <h5 className="card-text">
+                  {post.title}
+                </h5>
+                <p className="card-text">
+                  {post.content}
+                </p>
+                <p className="card-text">{post.tags}</p>
+                <div className="btn btn-danger">
+                  <i className="fa-solid fa-trash-can"></i> Elimina
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
+
+
       </div>
     </div>
   )
